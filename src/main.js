3a52ce0659e24c11c57a877b808204f32b686860
jinzhanye <jinzhan.ye@foxmail.com>
2018-07-16 16:02:28 +0800

    自定义主题

--- src/main.js
+++ src/main.js
@@ -1,11 +1,14 @@
 import Vue from 'vue';
 import App from './App';
 import router from './router';
-import './assets/theme-chalk/index.scss';
-import { install } from './index';
+// import './assets/theme-chalk/index.scss';
+// import { install } from './index';
+// install(Vue);
+import ElementUI from 'element-ui';
+import './assets/element-variables.scss';
+Vue.use(ElementUI);
 
 Vue.config.productionTip = false;
-install(Vue);
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
